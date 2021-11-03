434047cff75941a1fb6706a68ccfdf71c55ba16a
farzanaislam <farzanaislam2188@gmail.com>
2021-11-03 16:26:34 +0100

    love calculator project by java script

---
+++ app.js
@@ -0,0 +1,4 @@
+ let firstName=prompt("enter your first name:");
+ let secondName=prompt("enter your last name:");
+let number=Math.floor(Math.random()*100)+1;
+alert(firstName+" loves "+secondName+" = "+number +" % ");
