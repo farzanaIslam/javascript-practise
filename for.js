ce44550cfc074cc3435e5cb38d5b38bcfc420dd1
farzanaislam <farzanaislam2188@gmail.com>
2021-11-11 11:57:01 +0100

    ternary operator use in loop to find Even or Odd number

---
+++ for.js
@@ -0,0 +1,4 @@
+for(var i=1;i<=100;i++){
+    var result=i%2==0?"Even:"+i+"    ": "odd:"+i;
+    console.log(result);
+}
---
+++ shortAndOr.js
@@ -0,0 +1,3 @@
+var name="islam"
+var fullName=name||"farzana"
+console.log(fullName)
