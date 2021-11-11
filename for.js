6bfa177df9024776444d8d8af27a2198d6bce4ce
farzanaislam <farzanaislam2188@gmail.com>
2021-11-11 17:42:50 +0100

    only even number summation happend

--- for.js
+++ for.js
@@ -1,4 +1,15 @@
-for(var i=1;i<=100;i++){
-    var result=i%2==0?"Even:"+i+"    ": "odd:"+i;
-    console.log(result);
+// for(var i=1;i<=100;i++){
+//     var result=i%2==0?"Even:"+i+"    ": "odd:"+i;
+//     console.log(result);
+// }
+
+var sum=0,result;
+for(var i=1;i<=10;i++){
+if(i%2==0){
+    var sum=sum+i;
+    console.log(sum);
+}
+    
+   
+
 }
