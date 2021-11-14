c4fad91cb14614cd22ef274791b31f14afcdef9c
farzanaislam <farzanaislam2188@gmail.com>
2021-11-14 10:34:03 +0100

    how to find odd number in array summation  array data

--- arraySum.js
+++ arraySum.js
@@ -1,7 +1,15 @@
 var arr=[11,22,33,44,55,12,13,56,66];
 sum=0;
+// for(var i=0;i<arr.length;i++){
+//     if(arr[i]%2==0){
+//           console.log(arr[i]);
+//           sum=sum+arr[i];
+//     }
+//     // 
+// }
+
 for(var i=0;i<arr.length;i++){
-    if(arr[i]%2==0){
+    if(arr[i]%2!=0){
           console.log(arr[i]);
           sum=sum+arr[i];
     }
