4e49f28ef76cbfcf4bb6160c939e1584c6125950
farzanaislam <farzanaislam2188@gmail.com>
2021-11-14 10:31:53 +0100

    how to find even number in array summation  array data

--- arraySum.js
+++ arraySum.js
@@ -1,6 +1,10 @@
-var arr=[11,22];
+var arr=[11,22,33,44,55,12,13,56,66];
 sum=0;
 for(var i=0;i<arr.length;i++){
-    sum=sum+arr[i];
+    if(arr[i]%2==0){
+          console.log(arr[i]);
+          sum=sum+arr[i];
+    }
+    // 
 }
-console.log(sum);
+ console.log(sum);
