181416832bb705fdada3367f4e1dd3fbd00415b5
farzanaislam <farzanaislam2188@gmail.com>
2021-11-15 19:30:12 +0100

    use object dot notation and array notation use

---
+++ objectDotArrayNotation.js
@@ -0,0 +1,15 @@
+var student={
+    name:"aila",
+    id:01
+
+}
+student.section='A'
+student['group']='science'
+// var grade='B+'
+// student[grade]
+student.name='lila'
+student['name']='ira'
+var sec='grade'
+student[sec]='A'
+
+console.log(student)
