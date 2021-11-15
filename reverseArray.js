5faaec07e60c3998105ac23dc89716884df68430
farzanaislam <farzanaislam2188@gmail.com>
2021-11-15 11:05:12 +0100

    reverse in array use shortcut method reserve()

--- reverseArray.js
+++ reverseArray.js
@@ -1,9 +1,9 @@
 var arr =[1,2,3,4,5,6];
 
-for (let i = 0; i < (arr.length)/2; i++) {
-    var temp=arr[i];
-    arr[i] = arr[(arr.length-1)-i];
-    arr[(arr.length-1)-i]=temp;
+// for (let i = 0; i < (arr.length)/2; i++) {
+//     var temp=arr[i];
+//     arr[i] = arr[(arr.length-1)-i];
+//     arr[(arr.length-1)- i]=temp;
     
-}
-console.log(arr);
+// }
+console.log(arr.reverse());
