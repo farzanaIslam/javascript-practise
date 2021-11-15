6d040c8be82e47a4af7a4ec658c1082b14ff224f
farzanaislam <farzanaislam2188@gmail.com>
2021-11-15 12:04:09 +0100

    use object literal and constrator

--- objectLiteral.js
+++ objectLiteral.js
@@ -4,4 +4,15 @@ var female={
     height:5.3
 }
 female.skincolor="fair"
+var man=Object()
+man.name="mozammel",
+man.age=40,
+man.skincolor="fair"
+var kid=new Object()
+kid.name="taiba",
+kid.age=4,
+kid.skincolor="fair"
+
 console.log(female)
+console.log(man)
+console.log(kid)
