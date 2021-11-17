b3884ce8db8346bd65cd9ce7298766cbf048a899
farzanaislam <farzanaislam2188@gmail.com>
2021-11-17 19:58:40 +0100

    scope function

---
+++ scope.js
@@ -0,0 +1,32 @@
+var a=500;
+function dada(){
+    // var a=200
+
+    function sorwar(){
+        var a = 70
+
+    }
+    function anowar(){
+        var a= 55
+        
+    }
+    function baba(){
+        var a=30;
+
+    }
+    function nosru(){
+        var a=45
+        console.log(a)
+
+        
+    }
+    console.log(a)
+    nosru();
+    sorwar();
+    anowar();
+    baba();
+        
+    
+
+}
+dada();
