84f12c12820ac25ad82bbe8cff50b5ba7d761d82
farzanaislam <farzanaislam2188@gmail.com>
2021-11-17 20:30:03 +0100

    function can be stored in a variable

---
+++ function/firstClassFunction.js
@@ -0,0 +1,9 @@
+// firstclass function
+function add(a,b){
+    return a+b;
+}
+// 1) A function can be stored in a variable
+var result=add
+console.log(result(3,4))
+
+// 2) A function can be stored in Array
