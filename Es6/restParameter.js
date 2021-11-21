939c7fe6deb7c44b9bdf7b821eb95c5838fc260c
farzanaislam <farzanaislam2188@gmail.com>
2021-11-21 20:35:04 +0100

    rest parameter use in Es6

---
+++ Es6/restParameter.js
@@ -0,0 +1,9 @@
+function add (...number){
+ var sum =0
+ for (let i of number) {
+     sum+=i
+     
+ }
+ console.log(sum)
+}
+add(1,2,3,4,5,6,7,8,9,10);
--- Es6/spreadOperator.js
+++ Es6/spreadOperator.js
@@ -2,4 +2,5 @@
 var arr2=['mozammel','hoque'];
 var arr1=[...arr2,'farzana','islam'];
 
+
 console.log(arr1)
