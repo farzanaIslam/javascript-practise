7ffaf902bdc1178e3325dc03f0edaf55a7a02e76
farzanaislam <farzanaislam2188@gmail.com>
2021-11-22 18:51:07 +0100

    call back function in javascript

--- function/callback.js
+++ function/callback.js
@@ -2,16 +2,24 @@
 function sample(a,b,fun){
     var c= a+b
     var d= a-b
-    fun(c,d)
+ var result=   fun(c,d)
+ return result;
 
 }
+function sum(a,b)
+{
+    return a+b;
+}
+
+var result=sample(5,8,sum)
+console.log(result);
 sample(5,8,function(c,d)
 {
-    console.log(c+d);
+     console.log(c+d);
 })
 sample(5,8,function(c,d)
 {
-    console.log(c-d);
+    // console.log(c-d);
 })
 
 sample(5,8,function(c,d)
