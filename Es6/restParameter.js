beb85020e2bebe21e21335ba98661efae02ef6b6
farzanaislam <farzanaislam2188@gmail.com>
2021-11-28 11:10:34 +0100

    too much value receive by one parameter

--- Es6/restParameter.js
+++ Es6/restParameter.js
@@ -1,9 +1,11 @@
+// rest parameter er kaj holo array akare value receive kore
 function add (...number){
  var sum =0
  for (let i of number) {
      sum+=i
      
  }
- console.log(sum)
+ console.log(sum
+    )
 }
 add(1,2,3,4,5,6,7,8,9,10);
