c69bc7876d46cc1c90211e5462969a17f3828b06
farzanaislam <farzanaislam2188@gmail.com>
2021-12-04 11:06:53 +0100

    reduce function

---
+++ function/reduceCallback.js
@@ -0,0 +1,16 @@
+var arr=[2,200,11,4,33,6,7,32,66,39,29,79]
+// var sum=arr.reduce(function(pre,curr){
+//     return Math.max(pre,curr)
+// })
+// console.log(sum)
+function myReduce(arr,cb,acc){
+    for(var i=0;i<arr.length;i++){
+        acc=cb(acc,arr[i]);
+    }
+    return acc
+}
+var result=myReduce(arr,function(pre,curr){
+    return pre+curr
+
+},0)
+console.log(result)
