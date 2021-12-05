20117bb67b1513ed10ec56ede4dea3e8e81a2f7e
farzanaislam <farzanaislam2188@gmail.com>
2021-12-05 22:37:35 +0100

    one function return in another function in javascript solve in another way

--- function/higherOrder.js
+++ function/higherOrder.js
@@ -1,14 +1,28 @@
-function basa(furnitures){
-    function room(fur){
-        return 'lake of furnitures such as........'+furnitures+'     really!!!! now i need    ......'+fur
+// function basa(furnitures){
+//     function room(fur){
+//         return 'lake of furnitures such as........'+furnitures+'     really!!!! now i need    ......'+fur
+
+//     }
+//     return room
+// }
+
+// var arr=['bed','almira','fridge','sofa']
+// var result=basa(arr)
+// console.log(result)
+// var result2=result('sofa')
+
+// console.log(result2)
+
+function base(b){
+    return (p)=> {
+        var result=1
+        for(var i=0;i<b;i++){
+            result*=p
+        }
+        return result
 
     }
-    return room
 }
-
-var arr=['bed','almira','fridge','sofa']
-var result=basa(arr)
-console.log(result)
-var result2=result('sofa')
-
-console.log(result2)
+var base10=base(8)
+var paw=base10(8)
+console.log(paw)
