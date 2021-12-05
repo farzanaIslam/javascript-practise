dd2fb66568794627451cb5ec26b63795aa4d4ddb
farzanaislam <farzanaislam2188@gmail.com>
2021-12-05 10:21:45 +0100

    sorted object in array and callback function use in javascript

--- function/sort.js
+++ function/sort.js
@@ -1,4 +1,25 @@
 var arr=[2,5,-1,5,7,33,11,22,-8,-3]
+var person=[
+    {
+        name:'farzana',
+        age:31
+    },
+    {
+        name:'mahfuj',
+        age:23
+    },
+    {
+        name:'mahmud',
+        age:25
+    },
+    {
+        name:'kamrul',
+        age:34
+    },
+]
 // console.log(arr.sort())
 var result=arr.sort((a,b)=>{return a-b})
 console.log(result)
+
+var result1=person.sort((a,b)=>{return a.age-b.age})
+console.log(result1)
