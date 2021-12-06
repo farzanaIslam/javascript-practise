34861909588b37b0f7a412f69664eed35425c909
farzanaislam <farzanaislam2188@gmail.com>
2021-12-06 19:19:19 +0100

    recursive function using by summation

--- function/recursive.js
+++ function/recursive.js
@@ -10,11 +10,20 @@
 // }
 // console.log(add(5))
 
-function print(n){
-    if(n==0){
-        return 1
-    }
-   return n*print(n-1)
+// function print(n){
+//     if(n==0){
+//         return 1
+//     }
+//    return n*print(n-1)
     
+// }
+// console.log(print(5))
+
+
+function summation(n){
+    if(n==0){
+        return 0
+    }
+   return n+ summation(n-1)
 }
-console.log(print(5))
+console.log(summation(10))
